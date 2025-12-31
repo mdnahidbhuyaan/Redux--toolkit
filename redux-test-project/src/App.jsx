676a665489e75bd6ce23a745998bb0a91f580ac9
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2025-12-31 23:02:35 +0600

    api setup

--- redux-test-project/src/App.jsx
+++ redux-test-project/src/App.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { fetchGIF, fetchPhotos, fetchVideos } from './api/mediaApi'
 
 const App = () => {
+
   return (
     <div className='h-screen w-full text-white bg-gray-950'>
-    TG
+    <button className='bg-green-700 rounded-sm m-4 px-5 py-2 '
+    onClick={async()=>{
+      const data = await fetchPhotos("Cats")
+      console.log(data.results)
+    }}
+    >
+      Get Photos
+    </button>
+        <button className='bg-green-700 rounded-sm m-4 px-5 py-2 '
+    onClick={async()=>{
+      const data = await fetchVideos("Cats")
+      console.log(data.videos)
+    }}
+    >
+      Get Videos
+    </button>
+            <button className='bg-green-700 rounded-sm m-4 px-5 py-2 '
+    onClick={async()=>{
+      const data = await fetchGIF("Cats")
+      console.log(data.results)
+    }}
+    >
+      Get GIF
+    </button>
     </div>
   )
 }
