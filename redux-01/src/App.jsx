a59980a91cf1865849567b05a1b3901e560b8631
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2025-12-31 12:37:07 +0600

    unplash api coll

--- redux-01/src/App.jsx
+++ redux-01/src/App.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from "./redux/features/counterSlice";
 
 const App = () => {
-  return (
-    <div>
-      Home Page
-    </div>
-  )
-}
+  const dispatch = useDispatch();
 
-export default App
+  const count = useSelector((state) => state.counter.value);
+  const [num, setNum] = useState(5);
+
+  return (
+    <div className="">
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}
+      >
+        Decrement
+      </button>
+      <input
+      value={num}
+        type="Number"
+        onChange={(e) => {
+          setNum(e.target.value)
+        }}
+      />
+      <button
+        onClick={() => {
+          dispatch(incrementByAmount(Number(num)));
+        }}
+      >
+        Incres by Amount
+      </button>
+    </div>
+  );
+};
+
+export default App;
